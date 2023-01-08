04d8894c9765b98d5c73b9ba94afe105190898c2
Jmora-00 <121909501+Jmora-00@users.noreply.github.com>
2023-01-07 21:58:35 -0500

    about- pages

---
+++ pages/about/professional_experience.py
@@ -0,0 +1,12 @@
+import dash
+from dash import dcc, html, callback, Output, Input
+import plotly.express as px
+import dash_bootstrap_components as dbc
+
+dash.register_page(__name__, name='Professional Experience')
+
+layout = html.Div(
+    [
+        dcc.Markdown('# This will be the content of Page 1 and much more!')
+    ]
+)

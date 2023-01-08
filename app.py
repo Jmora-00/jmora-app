a88988f868b863ea1ba0a4219988096745218901
Jmora-00 <121909501+Jmora-00@users.noreply.github.com>
2023-01-07 21:56:20 -0500

    wepage structure
    main update with sidebar and tree files

--- app.py
+++ app.py
@@ -1,26 +1,115 @@
-from dash import Dash, dcc, html, Input, Output
-import os
+import dash
+from dash import html, dcc, Input, Output, State 
+import dash_bootstrap_components as dbc
+
+app = dash.Dash(__name__, use_pages=True, external_stylesheets=[dbc.themes.DARKLY])
+
+# the style arguments for the sidebar. We use position:fixed and a fixed width
+SIDEBAR_STYLE = {
+    "position": "fixed",
+    "top": 0,
+    "left": 0,
+    "bottom": 0,
+    "width": "16rem",
+    "padding": "2rem 1rem",
+}
+
+# the styles for the main content position it to the right of the sidebar and
+# add some padding.
+
+menu_drop_style = {'background-color': 'rgb(0,0,0,0.0)', 
+                     'textAlign': 'left',
+                     'color': 'white',
+                     'font-weight': 'bold',
+                     'border': '0px',
+                     'cursor':'pointer'
+                     }
+
+menu_link_style = {'background-color': 'rgb(0,0,0,0.0)', 
+                     'textAlign': 'left',
+                     'color': 'gray',
+                     'border': '0px',
+                     'cursor':'pointer'}
+
+sidebar = html.Div(
+    [
+        html.H2("Sidebar", className="display-4"),
+        html.Hr(),
+        html.P(
+            "A simple sidebar layout with navigation links", className="lead"
+        ),
+        dbc.Nav(
+            [
+                dbc.NavLink("Home", href="/",  style=menu_link_style),
+                dbc.NavLink("▾ About", active="exact", id="drop-menu-about", style=menu_drop_style),
+                dbc.Collapse(
+                    [
+                        dbc.NavLink(
+                            dash.page_registry['pages.about.professional_experience']["name"], 
+                            href=dash.page_registry['pages.about.professional_experience']["path"], 
+                            style=menu_link_style
+                        ),
+                        dbc.NavLink(
+                            dash.page_registry['pages.about.why_a_webpage']["name"],
+                            href=dash.page_registry['pages.about.why_a_webpage']["path"], 
+                            style=menu_link_style
+                        ),
+                        dbc.NavLink(
+                            dash.page_registry['pages.about.acknowledgments']["name"],
+                            href=dash.page_registry['pages.about.acknowledgments']["path"], 
+                            style=menu_link_style
+                        )
+                    ],
+                    id="collapse-menu-about", is_open=False
+                ),
+                dbc.NavLink("▾ Fixed Income", active="exact", id="drop-menu-fixed_income", style=menu_drop_style),
+                dbc.Collapse(
+                    [
+                        dbc.NavLink(
+                            dash.page_registry['pages.fixed_income.discussion_br']["name"],
+                            dash.page_registry['pages.fixed_income.discussion_br']["path"], 
+                            style=menu_link_style
+                        ),
+                        dbc.NavLink(
+                            dash.page_registry['pages.fixed_income.sample_app']["name"], 
+                            dash.page_registry['pages.fixed_income.sample_app']["path"],
+                            style=menu_link_style
+                        )
+                    ],
+                    id="collapse-menu-fixed_income", is_open=False
+                )
+            ],
+            vertical=True,
+            pills=True,
+        ),
+    ],
+    style=SIDEBAR_STYLE,
+)
+
+app.layout = html.Div([dcc.Location(id="url"), sidebar, dash.page_container])
+
+@app.callback(
+    Output("collapse-menu-about", "is_open"),
+    [Input("drop-menu-about", "n_clicks")],
+    [State("collapse-menu-about", "is_open")],
+)
+def toggle_collapse_about(n, is_open):
+    if n:       
+        return not is_open
+    return is_open
+
+@app.callback(
+    Output("collapse-menu-fixed_income", "is_open"),
+    [Input("drop-menu-fixed_income", "n_clicks")],
+    [State("collapse-menu-fixed_income", "is_open")],
+)
+def toggle_collapse_fixed_income(n, is_open):
+    if n:       
+        return not is_open
+    return is_open
 
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-
-app = Dash(__name__, external_stylesheets=external_stylesheets)
-
-server = app.server
-
-app.layout = html.Div([
-    html.H2('Hello World'),
-    dcc.Dropdown(['LA', 'NYC', 'MTL'],
-        'LA',
-        id='dropdown'
-    ),
-    html.Div(id='display-value')
-])
-
-@app.callback(Output('display-value', 'children'),
-                [Input('dropdown', 'value')])
-def display_value(value):
-    return f'You have selected {value}'
-
-if __name__ == '__main__':
-    app.run_server(debug=True)
+if __name__ == "__main__":
+    #app.run(debug=True)
+    app.run(debug=False)
+    

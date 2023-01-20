2563e01cdd90c920187e9f946c6cda3e899ba809
Jmora-00 <juanmora@berkeley.edu>
2023-01-20 01:50:08 -0500

    acknowledgments section update

--- pages/about/acknowledgments.py
+++ pages/about/acknowledgments.py
@@ -1,12 +1,26 @@
 import dash
 from dash import dcc, html, callback, Output, Input
-import plotly.express as px
 import dash_bootstrap_components as dbc
+
+CONTENT_STYLE = {
+    "margin-left": "20rem",
+    "margin-right": "22rem",
+    "padding": "6rem 1rem",
+}
 
 dash.register_page(__name__, name='Acknowledgments')
 
 layout = html.Div(
     [
-        dcc.Markdown('# This will be the content of Page 2 and much more!')
-    ]
+     html.Div("About > Acknowledgments", style={"font-style":"italic", "padding-bottom":'15px'}),
+     html.H3("Acknowledgments", style={'margin-top':'30px', 'margin-bottom':'10px', 'font-weight':'bold'}),
+     dcc.Markdown('''I ask a lot of questions. Thank you to all the people that still reply.  
+                  For this project, in particular:
+     '''),
+     dcc.Markdown('''
+                  * [Fernando Irarrazaval](https://www.linkedin.com/in/fernando-irarrazaval/): from databases and web hosting to graphic design. This guy knows everything.
+                  * [Ruchir Sharma](https://www.linkedin.com/in/sharma-ruchir/): his macroeconomic insightfulness is only matched by his proofreading skills.
+     '''),
+    ],
+    style=CONTENT_STYLE
 )
